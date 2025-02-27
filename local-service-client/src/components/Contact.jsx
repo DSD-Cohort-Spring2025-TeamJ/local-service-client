8f4a6f8e2f500f0d98849f7de52c130c714489bb
Roxy Rodriguez-Becker <roxy.rodriguez@gmail.com>
2025-02-26 20:12:29 -0600

    Simple footer.

---
+++ local-service-client/src/components/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <>
+        <h3><a href='mailto:thepragmaticplumber@pragmaticplumbing.com'>Contact us</a></h3>
+        <h3>Call us at 555-555-5555</h3>
+        </>
+    )
+}
---
+++ local-service-client/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright 2025 The Pragmatic Plumber</p>
+            <div className="social-icons">
+
+            </div>
+
+        </footer>
+
+    )
+
+
+}
+
+export default Footer;
