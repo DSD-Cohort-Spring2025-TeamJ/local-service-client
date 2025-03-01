447d72490a03473eaf2e0973b6e11ce30278a185
Roxy Rodriguez-Becker <roxy.rodriguez@gmail.com>
2025-03-01 06:29:43 -0600

    Fixed some footer naming and css.

--- local-service-client/src/components/Footer.jsx
+++ local-service-client/src/components/Footer.jsx
@@ -1,10 +1,10 @@
 export default function Footer() {
     return (
-        <>
+        <footer>
         <p className="copyright">Copyright 2025 The Pragmatic Plumber</p>
         <div className = "socials">
 
         </div>
-        </>
+        </footer>
     )
 }
