c940245bf6f3f0cc7496aa24f0b446952e2a2cb9
Prescott <38223423+Lockeheede@users.noreply.github.com>
2025-03-05 09:02:15 -0600

    Created fetch request functionality (#7)
    * Created fetch request functionality
    
    * Added BE (josh) HTTP for response request. His code needs to be pulled for the HTTP to work
    
    * Added GET request and separate AppointmentsList.jsx file. Added josh's URL for getting data.
    
    ---------
    
    Co-authored-by: Donte Ladatto <98425707+dawnteigh@users.noreply.github.com>

--- local-service-client/src/App.jsx
+++ local-service-client/src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Switch } from "react-router-dom";
 import Nav from "./Nav";
 import Login from "./Login";
 import Signup from "./Signup";
+import ClientInfoForm from "./components/ClientInfoForm";
+import AppointmentsList from "./components/AppointmentsList";
 import Dashboard from "./Dashboard";
 import Footer from "/src/components/Footer.jsx"
 import Services from "/src/components/Services.jsx"
@@ -19,6 +21,7 @@ function App() {
           <Route exact path="/signup" render={(props) => <Signup {...props} />} />
           <Route exact path="/" render={(props) => <Dashboard {...props} />} />
         </Switch>
+
         <Services />
         <Footer />
       </div>
