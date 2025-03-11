267e9e4fd71396f566b1119ef56e09f5bcbd7283
Donte Ladatto <donte279@gmail.com>
2025-03-10 21:03:25 -0500

    ramshackle conditional rendering

--- src/pages/ServiceRequest.jsx
+++ src/pages/ServiceRequest.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Services from '../components/Services'
 import AppointmentScheduler from '../components/AppointmentScheduler'
 import ClientInfoForm from '../components/ClientInfoForm'
+import { Context } from '../context/Context'
 
 const ServiceRequest = () => {
+
+  const { selectedService, appointment } = useContext(Context)
+  const { service_id, date, time_slot } = appointment
+  //TODO: cleaner conditional rendering
+
   return (
     <>
-      <Services />
-      <AppointmentScheduler />
-      <ClientInfoForm />
+      {!service_id && <Services />}
+      {selectedService && (!date || !time_slot) && <AppointmentScheduler />}
+      {(time_slot && date) && <ClientInfoForm />}
     </>
   )
 }
