fb2d5d3bc9d5ff98e898c2186fd356b505cd9230
Donte Ladatto <donte279@gmail.com>
2025-03-10 21:04:54 -0500

    render appointments from selectedService state

--- src/components/AppointmentScheduler.jsx
+++ src/components/AppointmentScheduler.jsx
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import add from 'date-fns/add'
-
-const currentDate = new Date()
-const todayString = currentDate.toISOString().split('T')[0]
-const nextWeekString = add(currentDate, { weeks: 1 }).toISOString().split('T')[0]
-const times = ["9:00", "10:00", "11:00", "12:00"]
-
-const renderTimes = times.map(t => { return <option value={t}>{t}</option> })
-const renderTimeButtons = times.map(t => { return <button>{t}</button> })
-
-
+import { Context } from '../context/Context';
 
 function AppointmentScheduler() {
+
+  const { selectedService, appointment, setAppointment } = useContext(Context);
+
+  const currentDate = new Date()
+  const todayString = currentDate.toISOString().split('T')[0]
+  const nextWeekString = add(currentDate, { weeks: 1 }).toISOString().split('T')[0]
+
+  const [day, setDay] = useState(todayString)
+
+  const sortByTime = (arr) => {
+    return arr.sort((a, b) => {
+      const dateA = new Date(`01/01/2000 ${a}`);
+      const dateB = new Date(`01/01/2000 ${b}`);
+      return dateA - dateB;
+    });
+  }
+
+  const times = () => {
+    let slots = [];
+    let arrays = Object.values(selectedService.availableTimeSlotsByTechnician)
+    arrays.map(t => t[day].map(d => slots.push(d)))
+    return sortByTime([...new Set(slots)])
+  }
+
+  const handleDayChange = (e) => {
+    setDay(e.target.value)
+    setAppointment({
+      ...appointment,
+      date: e.target.value
+    })
+  }
+  const handleSlotSelection = (e) => {
+    setAppointment({
+      ...appointment,
+      time_slot: e.target.value
+    })
+  }
+  const renderTimeButtons = times().map((t, i) => { return <button key={i} value={t} onClick={handleSlotSelection}>{t}</button> })
+
   return (
     <div>
-      <input type="date" min={todayString} max={nextWeekString} />
-      <select id="service">
-        <option value="">Select an appointment</option>
-        {renderTimes}
-      </select>
+      <p>Estimated completion time: {selectedService.estimated_time} minutes</p>
+      <input type="date" min={todayString} max={nextWeekString} onChange={handleDayChange} />
       <br />
       {renderTimeButtons}
     </div>
