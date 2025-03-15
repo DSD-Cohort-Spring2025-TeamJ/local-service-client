434684dcd5924998e67045ca18bdbff7b52ba396
Donte Ladatto <donte279@gmail.com>
2025-03-14 19:35:49 -0500

    fix styling errors

--- src/pages/ServiceRequest.jsx
+++ src/pages/ServiceRequest.jsx
@@ -67,21 +67,21 @@ const ServiceRequest = () => {
         }}
       >
         {!isFirstStep && (
-          <Button 
-          className="bg-[#4BCE4B] rounded-[1rem] no-underline px-[5px] py-[5px] w-[100px] h-[25px]
+          <Button
+            className="bg-[#4BCE4B] rounded-[1rem] no-underline px-[5px] py-[5px] w-[100px] h-[25px]
           shadow-[inset_0_-25px_18px_-14px_rgba(1,185,38,0.35),0_1px_2px_rgba(1,177,30,0.35),0_2px_4px_rgba(3,194,79,0.35),0_4px_8px_rgba(1,192,17,0.35),0_8px_16px_rgba(1,119,42,0.35),0_16px_32px_rgba(2,199,78,0.35)]
-          text-[#4B4B4B] font-sans border-[1px] border-[#4BCE4B]"
-          active:scale-90
-          type="button" 
-          onClick={back}>
-            Back
-          </Button>
+          text-[#4B4B4B] font-sans border-[1px] border-[#4BCE4B]
+          active:scale-90"
+            type="button"
+            onClick={back}
+            text="Back" />
         )}
         <Button className="bg-[#4BCE4B] rounded-[1rem] no-underline px-[5px] py-[5px] w-[100px] h-[25px] mb-2 mr-2
         shadow-[inset_0_-25px_18px_-14px_rgba(1,185,38,0.35),0_1px_2px_rgba(1,177,30,0.35),0_2px_4px_rgba(3,194,79,0.35),0_4px_8px_rgba(1,192,17,0.35),0_8px_16px_rgba(1,119,42,0.35),0_16px_32px_rgba(2,199,78,0.35)]
-        text-[#4B4B4B] font-sans border-[1px] border-[#4BCE4B]"
-        active:scale-90
-        type="submit">{isLastStep ? "Finish" : "Next"}</Button>
+        text-[#4B4B4B] font-sans border-[1px] border-[#4BCE4B]
+        active:scale-90"
+          type="submit"
+          text={isLastStep ? "Finish" : "Next"} />
       </div>
     </form>
 
