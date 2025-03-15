cf0c3ab874f90b5b32c4050c9aa587a66385629e
Roxy Rodriguez-Becker <roxy.rodriguez@gmail.com>
2025-03-15 16:52:07 -0500

    favicon, title, header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
 import Logo from '/src/components/Logo.jsx'
 export default function Footer() {
     return (
-        <footer className="flex flex-row justify-between items-center p-4 align-bottom">
+        <footer className="flex flex-row justify-between items-center p-4 align-bottom bg-[#F0F0F0] text-green-800 w-full">
                 <div>
                 <p>200 Agile Avenue</p>
                 <p>Plungerville, TX</p>
                 </div>
  
-                <p className="copyright">Copyright 2025 The Pragmatic Plumber</p>
+                <p>Copyright 2025 The Pragmatic Plumber</p>
      
   
                 <p><a href="mail to:">ThePragPlumb@Plumbing.com</a></p>
