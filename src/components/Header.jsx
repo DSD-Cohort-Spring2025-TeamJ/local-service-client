7eea63d2cb5e60af593bf8f1c0d95c8e70ca4837
Donte Ladatto <donte279@gmail.com>
2025-03-17 15:36:35 -0500

    conditionally render book button with useLocation hook

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,27 @@
 import Button from '/src/components/Button.jsx'
 import Logo from '/src/components/Logo.jsx'
-import { useHistory } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 
 export default function Header({ setOpen }) {
 
     const history = useHistory();
+    const location = useLocation();
 
     return (
         <div className="flex flex-col items-center pl-8">
             <div className="flex flex-row gap-10 items-center pl-8">
                 <Logo />
-                <Button
-                    className="book-online bg-[#4BCE4B] rounded-[3rem] no-underline px-[5px] py-[5px] w-[200px] h-[75px] text-2xl
+                {
+                    location.pathname === "/admin" ? null :
+                        <Button
+                            className="book-online bg-[#4BCE4B] rounded-[3rem] no-underline px-[5px] py-[5px] w-[200px] h-[75px] text-2xl
                 shadow-[inset_0_-25px_18px_-14px_rgba(1,185,38,0.35),0_1px_2px_rgba(1,177,30,0.35),0_2px_4px_rgba(3,194,79,0.35),0_4px_8px_rgba(1,192,17,0.35),0_8px_16px_rgba(1,119,42,0.35),0_16px_32px_rgba(2,199,78,0.35)]
                 text-black-100 font-sans font-extrabold border-[1px] border-[#4BCE4B]
                 hover:bg-green-700 hover:text-white
                 active:scale-90 border-[#005701]"
-                    text="BOOK ONLINE"
-                    onClick={() => setOpen(true)}
-                />
+                            text="BOOK ONLINE"
+                            onClick={() => setOpen(true)}
+                        />}
                 <Button
                     className="bg-gray-300 rounded-[2rem] no-underline px-[5px] py-[5px] w-[100px] h-[35px]
                 shadow-[inset_0_-25px_18px_-14px_rgba(1,185,38,0.35),0_1px_2px_rgba(1,177,30,0.35),0_2px_4px_rgba(3,194,79,0.35),0_4px_8px_rgba(1,192,17,0.35),0_8px_16px_rgba(1,119,42,0.35),0_16px_32px_rgba(2,199,78,0.35)]
@@ -26,7 +29,7 @@ export default function Header({ setOpen }) {
                 hover:bg-green-700 hover:text-white
                 active:scale-90 border-[#005701] ml-95"
                     text="ADMIN"
-                    onClick={() => history.push('/dashboard')}
+                    onClick={() => history.push('/admin')}
                 />
             </div>
 
