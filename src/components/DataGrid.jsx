9f2455aa7fd4907a319547ae013bf9244e74ef4c
Donte Ladatto <donte279@gmail.com>
2025-03-19 17:01:23 -0500

    add theme and height prop

--- src/components/DataGrid.jsx
+++ src/components/DataGrid.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import { AgGridReact } from 'ag-grid-react';
+import { themeQuartz } from "ag-grid-community";
 
-const DataGrid = ({ rowData, colDefs }) => {
+const DataGrid = ({ rowData, colDefs, height }) => {
+
+  const theme = themeQuartz.withParams({
+    backgroundColor: 'white',
+    foregroundColor: 'black',
+    headerTextColor: '#f0f0f0',
+    headerBackgroundColor: '#00BB03',
+    oddRowBackgroundColor: 'rgb(0, 0, 0, 0.03)',
+    headerColumnResizeHandleColor: 'black',
+  });
+
   return (
-    <div style={{ height: 500 }}>
+    <div style={{ height: height }}>
       <AgGridReact
         rowData={rowData}
         columnDefs={colDefs}
+        theme={theme}
       />
     </div>
   )
