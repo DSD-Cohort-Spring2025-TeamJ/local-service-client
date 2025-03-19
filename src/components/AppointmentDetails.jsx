e201b7508f4ad7e6c2d6eb352eb41bcc59e5b34b
Donte Ladatto <donte279@gmail.com>
2025-03-19 16:57:38 -0500

    Add AG Grid to AppointmentDetails

--- src/components/AppointmentDetails.jsx
+++ src/components/AppointmentDetails.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import DataGrid from "./DataGrid";
 
 function AppointmentDetails({ appointment, setAppointment }) {
   if (!appointment) return <h1>Loading...</h1>
@@ -35,18 +36,11 @@ function AppointmentDetails({ appointment, setAppointment }) {
       })
   }
 
-  const itemTable = appointment.items.map(i => {
-    return (
-      <tr key={i.item_id} className="text-center text-sm" style={{ padding: "10px" }}>
-        <td className="px-2 py-2">{i.item.item_name}</td>
-        <td className="px-2 py-2">{i.qty_needed}</td>
-        <td className="px-2 py-2">{i.outOfStock === false ? "Yes" : "No"}</td>
-        <td className="px-2 py-2">{i.item.unit_price}</td>
-        {/* Total Cost */}
-      </tr>
-    );
-  }
-  );
+  const apptRowData = [{ "Client Name": client_name, "Phone": client_phone, "Email": client_email, "Address": location, "Comments": issue_description, "Date Selected": start_time?.split("T")[0], "Time Selected": end_time, "Estimated Time": estimated_time, "Status": status }]
+  const apptColDefs = [{ field: "Client Name" }, { field: "Phone" }, { field: "Email" }, { field: "Address" }, { field: "Comments" }, { field: "Date Selected" }, { field: "Time Selected" }, { field: "Estimated Time" }, { field: "Status" }]
+
+  const itemRowData = appointment.items.map(i => ({ "Name": i.item.item_name, "Qty Needed": i.qty_needed, "In Stock": i.outOfStock ? "No" : "Yes", "Unit Price": i.item.unit_price }))
+  const itemColDefs = [{ field: "Name" }, { field: "Qty Needed" }, { field: "In Stock" }, { field: "Unit Price" }]
   return (
     <>
       <div>
@@ -59,52 +53,11 @@ function AppointmentDetails({ appointment, setAppointment }) {
         active:scale-90"
           onClick={() => setAppointment(null)}>X</button>
         <h1>Client Information</h1>
-        <table className="min-w-full table-auto border-collapse">
-          <thead>
-            <tr className="text-center text-sm">
-              <th className="px-2 py-2">Client Name</th>
-              <th className="px-2 py-2">Phone</th>
-              <th className="px-2 py-2">Email</th>
-              <th className="px-2 py-2">Address</th>
-              <th className="px-2 py-2">Comments</th>
-              <th className="px-2 py-2">Date Selected</th>
-              <th className="px-2 py-2">Time Selected</th>
-              <th className="px-2 py-2">Estimated Time</th>
-              <th className="px-2 py-2">Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="px-2 py-2">{client_name}</td>
-              <td className="px-2 py-2">{client_phone}</td>
-              <td className="px-2 py-2">{client_email}</td>
-              <td className="px-2 py-2">{location}</td>
-              <td className="px-2 py-2">{issue_description}</td>
-              {/* <td className="px-2 py-2">{}</td> */}
-              <td className="px-2 py-2">{start_time?.split("T")[0]}</td>
-              <td className="px-2 py-2">{end_time}</td>
-              <td className="px-2 py-2">{estimated_time}</td>
-              <td className="px-2 py-2">{status}</td>
-            </tr>
-          </tbody>
-        </table>
+        <DataGrid rowData={apptRowData} colDefs={apptColDefs} height={100} />
       </div>
       <div>
         <h1>Inventory Check</h1>
-        <table className="min-w-full table-auto border-collapse">
-          <thead>
-            <tr className="text-center text-sm">
-              <th className="px-2 py-2">Name</th>
-              <th className="px-2 py-2">Qty Needed</th>
-              <th className="px-2 py-2">In Stock</th>
-              <th className="px-2 py-2">Unit Price</th>
-              {/* <th className="px-2 py-2">Total Cost</th> */}
-            </tr>
-          </thead>
-          <tbody>
-            {itemTable}
-          </tbody>
-        </table>
+        <DataGrid rowData={itemRowData} colDefs={itemColDefs} height={300} />
         <h1>Notes</h1>
         <textarea className="w-full h-32 border" value={note} onChange={(e) => setNote(e.target.value)}></textarea>
         <br />
