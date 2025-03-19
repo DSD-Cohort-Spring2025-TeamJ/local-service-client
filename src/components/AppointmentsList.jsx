75b677eb1824d7532ed1adbce0d6b08a78088b53
Donte Ladatto <98425707+dawnteigh@users.noreply.github.com>
2025-03-19 17:32:29 -0500

    Admin can Accept or Decline Jobs, See Inventory Stock indicator, and Save Notes + AG Grid tables (#71)
    * disable decline, textarea border
    
    * button disabled style
    
    * write status change handler
    
    * control textarea content with state
    
    * discard button clears notes
    
    * write function to save notes to the appointment
    
    * separate buttons, add 'Status' table header
    
    * reorder buttons, rename save button
    
    * Merge admin flow and AG Grid branches together (#70)
    
    * add AG Grid React
    
    * import and register AG Grid modules
    
    * create DataGrid component
    
    * use AG Grid for appointment list
    
    * Add AG Grid to AppointmentDetails
    
    * add status update handler and button disable logic
    
    * Functional note saving
    
    * explicit grid height
    
    * add theme and height prop
    
    * remove button padding
    
    * control disabled status with state
    
    * Set new appointment status and disable buttons after request
    
    * fix default disabled logic

--- src/components/AppointmentsList.jsx
+++ src/components/AppointmentsList.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import DataGrid from "./DataGrid";
 
 function AppointmentsList({ setAppointment }) {
   const [appointments, setAppointments] = useState([]);
@@ -52,53 +53,26 @@ function AppointmentsList({ setAppointment }) {
     }
   };
 
-
-  const appointmentTable = appointments.map((appointment) => {
+  const customButton = props => {
     return (
-      <tr key={appointment.appointment_id}>
-        <td className="px-2 py-2">{appointment.appointment_id}</td>
-        <td className="px-2 py-2">{appointment.service_id.service_id}</td>
-        <td className="px-2 py-2">{appointment.client_note}</td>
-        <td className="px-2 py-2">{appointment.issue_description}</td>
-        <td className="px-2 py-2">{appointment.location}</td>
-        <td className="px-2 py-2">{appointment.admin_note}</td>
-        <td className="px-2 py-2">{appointment.created_at}</td>
-        <td className="px-2 py-2">{appointment.updated_at}</td>
-        <td className="px-2 py-2">{appointment.estimated_time}</td>
-        <td className="px-2 py-2">{appointment.status}</td>
-        <td className="px-2 py-2">
-          <button
-            onClick={() => handleAppointmentClick(appointment.appointment_id)}
-            className="bg-[#4BCE4B] rounded-[1rem] no-underline px-[5px] py-[5px] w-[100px] h-[25px] mb-2 mr-2
-        shadow-[inset_0_-25px_18px_-14px_rgba(1,185,38,0.35),0_1px_2px_rgba(1,177,30,0.35),0_2px_4px_rgba(3,194,79,0.35),0_4px_8px_rgba(1,192,17,0.35),0_8px_16px_rgba(1,119,42,0.35),0_16px_32px_rgba(2,199,78,0.35)]
-        text-[#4B4B4B] font-sans border-[1px] border-[#4BCE4B]
-        active:scale-90">
-            View Details
-          </button>
-        </td>
-      </tr>
-    );
-  });
+      <button
+        onClick={() => handleAppointmentClick(props.data["Appointment ID"])}
+        className="button">
+        View Details
+      </button>)
+  }
+  const rowData = appointments.map(a => ({ "Appointment ID": a.appointment_id, "Service ID": a.service_id.service_id, "Description": a.issue_description, "Location": a.location, "Admin Note": a.admin_note, "Estimated Time": a.estimated_time, "Status": a.status }))
+  const colDefs = [{ field: "Appointment ID" }, { field: "Service ID" }, { field: "Description" }, { field: "Location" }, { field: "Admin Note" }, { field: "Estimated Time" }, { field: "Status" }, {
+    field: "actions",
+    headerName: "Actions",
+    cellRenderer: customButton
+
+  }]
+
   return (
     <div>
       <h1>Appointments</h1>
-      <table className="min-w-full table-auto border-collapse">
-        <thead>
-          <tr className="text-center text-sm">
-            <th className="px-2 py-2">Appointment ID</th>
-            <th className="px-2 py-2">Service ID</th>
-            <th className="px-2 py-2">Client Note</th>
-            <th className="px-2 py-2">Description</th>
-            <th className="px-2 py-2">Location</th>
-            <th className="px-2 py-2">Admin Note</th>
-            <th className="px-2 py-2">Created At</th>
-            <th className="px-2 py-2">Updated At</th>
-            <th className="px-2 py-2">Estimated Time</th>
-            <th className="px-2 py-2">Status</th>
-          </tr>
-        </thead>
-        <tbody>{appointmentTable}</tbody>
-      </table>
+      <DataGrid colDefs={colDefs} rowData={rowData} height={500} />
     </div>
   );
 }
