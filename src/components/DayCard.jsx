a290cfd3d21ee5a1efd00353b9be3d1ab7c8e390
Donte Ladatto <donte279@gmail.com>
2025-03-20 13:53:13 -0500

    update props, format date with date-fns

--- src/components/DayCard.jsx
+++ src/components/DayCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Button from './Button'
+import { format } from 'date-fns'
 
 const DayCard = ({ date, slots, buttonFunction }) => {
 
-  const slotButtons = slots.map(s => <Button text={s} onClick={buttonFunction} />)
+  const slotButtons = slots.map((s, i) => <Button key={i} id={date} value={s} className="details-button" text={s} onClick={buttonFunction} />)
   return (
     <div>
-      <h2>{date}</h2>
+      {/* for some reason, date-fns format() returns one day previous with a hyphenated date, so we switch to slashes here */}
+      <h2>{format(date.replace(/-/g, '/'), 'EEEE, MMMM do')}</h2>
       {slotButtons}
     </div>
   )
