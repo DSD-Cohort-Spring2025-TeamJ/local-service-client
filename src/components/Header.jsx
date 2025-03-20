44ac8ef0bbf26b376a5763ab05da4e4f66a1f724
Joshua Ow <joshuaow@gmail.com>
2025-03-20 09:52:38 -0500

    deploy fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,29 +6,24 @@ import { useLocation } from "react-router-dom";
 export default function Header({ setOpen }) {
   const location = useLocation();
 
-    return (
-        <div className="flex flex-col items-center pl-8 w-full">
-            <div className="flex flex-row gap-30 items-center pl-8 w-full">
-                <Logo />
-                <Button
-                    className="main-button book-online w-[200px] h-[75px] text-2xl p-10 
+  return (
+    <div className="flex flex-col items-center pl-8 w-full">
+      <div className="flex flex-row gap-30 items-center pl-8 w-full">
+        <Logo />
+        <Button
+          className="main-button book-online w-[200px] h-[75px] text-2xl p-10 
 bg-[#4BCE4B]"
-                    text="BOOK ONLINE"
-                    onClick={() => setOpen(true)}
-                />
-                
+          text="BOOK ONLINE"
+          onClick={() => setOpen(true)}
+        />
 
-                <Button
-                    className="main-button bg-gray-300 rounded-[2rem] w-[90px] h-[30px] p-3 text-lg flex items-center text-center"
-                    text="ADMIN"
-                    onClick={() => history.push('/admin')}
-                />
-
-            </div>
-
-        </div>
+        <Button
+          className="main-button bg-gray-300 rounded-[2rem] w-[90px] h-[30px] p-3 text-lg flex items-center text-center"
+          text="ADMIN"
+          onClick={() => history.push("/admin")}
+        />
       </div>
-    </header>
+    </div>
   );
 }
 
