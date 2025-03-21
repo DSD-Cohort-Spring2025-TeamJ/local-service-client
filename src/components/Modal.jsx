43632f2ec4eb4662e1d79038c1beae1b586fac80
Donte Ladatto <98425707+dawnteigh@users.noreply.github.com>
2025-03-21 18:27:30 -0500

    Modal and details button css fixes (#89)
    * add max heights to modal and button grids
    
    * add back details-button class
    
    * edit service button focus

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,7 +5,7 @@ const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
   return (
     <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content h-[75%]" onClick={(e) => e.stopPropagation()}>
+      <div className="modal-content max-h-[75%]" onClick={(e) => e.stopPropagation()}>
         <div className="flex justify-end">
           <Button
             className="main-button w-[20px] h-[20px] bg-[#4BCE4B] px-[5px] py-[5px] text-[#4B4B4B] text-sm 
