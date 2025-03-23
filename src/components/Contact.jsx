99a5445e974801e0a78948fd5060b95fda2a3e2e
Joshua Ow <joshuaow@gmail.com>
2025-03-23 14:35:15 -0500

    Footer round 2 (#110)
    * moved gallery photos to public folder
    
    * centered with icons
    
    * landing page and header additions (#109)
    
    ---------
    
    Co-authored-by: Roxy Rodriguez-Becker <roxy.rodriguez@gmail.com>
    Co-authored-by: Roxana Rodriguez-Becker <126708010+roxyrodbeck@users.noreply.github.com>

---
+++ src/components/Contact.jsx
@@ -0,0 +1,9 @@
+
+export default function Contact() {
+    return (
+        <div className="flex flex-col justify-center rounded-lg items-center shadow-xl shadow-green-500/50 m-10 p-1">
+        <h2>Call today!</h2>
+        <h1>(555)555-5555</h1>
+        </div>
+    )
+}
---
+++ src/components/Reviews.jsx
@@ -0,0 +1,16 @@
+
+export default function Reviews() {
+return (
+<div className="carousel carousel-vertical -mt-20 w-[250px] h-[100px] shadow-xl shadow-green-500/50 m-5 p-2">
+  <div className="carousel-item h-full mb-4">
+    <img src="/review-1.png" />
+  </div>
+  <div className="carousel-item h-full mb-4">
+    <img src="/review-2.png" />
+  </div>
+  <div className="carousel-item h-full mb-4">
+    <img src="/review-3.png" />
+  </div>
+</div>
+)
+}
