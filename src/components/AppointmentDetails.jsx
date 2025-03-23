d73853d5c975e0fd949a85f8e5b502322227dae8
Joshua Ow <joshuaow@gmail.com>
2025-03-22 19:15:23 -0500

    fixed appointment details linter error

--- src/components/AppointmentDetails.jsx
+++ src/components/AppointmentDetails.jsx
@@ -3,17 +3,6 @@ import { useState } from "react";
 import { format } from "date-fns";
 
 function AppointmentDetails({ appointment, setAppointment }) {
-  const serviceName = service_id?.service_name || "Service";
-  const items = appointment.items || [];
-  const [showDeclineConfirm, setShowDeclineConfirm] = useState(false);
-  const [note, setNote] = useState(admin_note ?? "");
-  const [disabled, setDisabled] = useState(
-    status === "ACCEPTED" || status === "REJECTED"
-  );
-  const [notification, setNotification] = useState("");
-
-  if (!appointment) return <h1>Loading...</h1>;
-
   const {
     appointment_id,
     client_name,
@@ -28,6 +17,17 @@ function AppointmentDetails({ appointment, setAppointment }) {
     admin_note,
     service_id,
   } = appointment.appointment;
+
+  const serviceName = service_id?.service_name || "Service";
+  const items = appointment.items || [];
+  const [showDeclineConfirm, setShowDeclineConfirm] = useState(false);
+  const [note, setNote] = useState(admin_note ?? "");
+  const [disabled, setDisabled] = useState(
+    status === "ACCEPTED" || status === "REJECTED"
+  );
+  const [notification, setNotification] = useState("");
+
+  if (!appointment) return <h1>Loading...</h1>;
 
   const handleUpdateStatus = async (newStatus) => {
     fetch(
