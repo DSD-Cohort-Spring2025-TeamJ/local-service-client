c3d30e2010e1092609ed83488498628a47e69281
Joshua Ow <joshuaow@gmail.com>
2025-03-25 16:54:12 -0500

    escaped the quotes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -110,7 +110,9 @@ export function TestimonialCarousel() {
             transition={{ duration: 0.6 }}
             className="text-center"
           >
-            <p className="text-gray-700 mb-2">"{reviews[index].text}"</p>
+            <p className="text-gray-700 mb-2">
+              &quot;{reviews[index].text}&quot;
+            </p>
             <p className="text-green-600 font-semibold">
               - {reviews[index].author}
             </p>
