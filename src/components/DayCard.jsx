e726f676c8e74096a583ae1e58d0e32ad9dc93b1
Joshua Ow <joshuaow@gmail.com>
2025-03-26 22:15:47 -0500

    Ticket 146 (#148)
    * escaped the quotes
    
    * ticket 146 and 147 complete

--- src/components/DayCard.jsx
+++ src/components/DayCard.jsx
@@ -18,10 +18,10 @@ const DayCard = ({ date, slots, buttonFunction, selectedSlot }) => {
               key={i}
               type="button"
               onClick={() => buttonFunction(date, s.start, s.end)}
-              className={`px-4 py-2 rounded-full text-sm shadow-sm transition ${
+              className={`px-4 py-2 rounded-full text-sm shadow-sm hover:cursor-pointer transition ${
                 isSelected
-                  ? "bg-green-400 text-white scale-105"
-                  : "bg-green-300 text-white hover:bg-green-600"
+                  ? "bg-green-400 ring-2 ring-green-900 text-black scale-105"
+                  : "bg-green-300 text-black hover:bg-green-400"
               }`}
             >
               {s.start} - {s.end}
